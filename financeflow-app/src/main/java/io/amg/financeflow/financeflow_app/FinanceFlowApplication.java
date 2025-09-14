c613db1a3b05f1ccdbf5f5674edc06d222d45732
KaizenG <desiigner4074@gmail.com>
2025-09-14 14:02:18 +0800

    first setup(dependencies, partial structure, database pod pero partial pasad)

---
+++ financeflow-app/src/main/java/io/amg/financeflow/financeflow_app/FinanceFlowApplication.java
@@ -0,0 +1,13 @@
+package io.amg.financeflow.financeflow_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceFlowApplication.class, args);
+	}
+
+}
